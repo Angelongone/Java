6be727978234488bf32e8d70195d9bdb7597d682
Angelong <53002248+Angelongone@users.noreply.github.com>
2019-09-19 11:17:34 +0800

    Update 带空格的三角形.java

--- 练习/For循环/带空格的三角形.java
+++ 练习/For循环/带空格的三角形.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 public class Temp {
 	public static void main(String[] args) {
-		int n = 3;
+		Scanner scan = new Scanner(System.in);
+		System.out.print("请输入行数：");
+		int n = scan.nextInt();
+//		int n = 15;
 		for(int i = 0 ; i < n ; i++) {
-			for(int j = 0 ; j < 2*n; j++) {
-				if(j > n+1){
+			for(int j = 0 ; j <= 2*n; j++) {
+				if(j > 2*n-1){
 					System.out.println();
 				}else if(j < n-i-1 || j >= n+i+1){
 					System.out.print(" ");
